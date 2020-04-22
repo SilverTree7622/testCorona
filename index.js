20e1390d3a17a3db3ba3f4335faae51d101a71d4
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2020-04-22 21:14:46 +0900

    fix some detailed

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000; // 1
+var port = process.env.PORT || 3000;
 
 app.use(express.static('client'));
 app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/js/main.js');
-  // res.sendFile(__dirname + '/css/main.css');
-  // res.sendFile(__dirname + '/favicon.ico');
-  res.sendFile(__dirname + '/index.html');
+	// res.sendFile(__dirname + '/js/main.js');
+	// res.sendFile(__dirname + '/css/main.css');
+	// res.sendFile(__dirname + '/favicon.ico');
+
+	// __dirname => auto generated var (relative path)
+	res.sendFile(__dirname + '/index.html');
 });
 app.listen(port, () => {
-  console.log('__dirname:', __dirname);
-  console.log('Example app listening at http://localhost:' + port)
+	console.log('__dirname:', __dirname);
+	console.log('Example app listening at http://localhost:' + port)
 });
 
