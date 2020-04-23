836428eff3fba7af8d872f98483f0138e5e677c6
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2020-04-23 16:08:28 +0900

    temp2

--- index.js
+++ index.js
@@ -11,48 +11,29 @@ var whiteList = [
 	'https://www.mohw.go.kr/react/popup_200128_4.html'
 ];
 var corsOption = {
-	// origin: function (_origin, _callback) {
-	// 	if (whiteList.indexOf(_origin) !== -1) {
-	// 		_callback(null, true)
-	// 	} else {
-	// 		_callback(new Error('Not allowed by CORS'))
-	// 	}
-	// }
-	origin:	whiteList,
-	default: 'https://www.mohw.go.kr/react/popup_200128.html'
+	origin: function (_origin, _callback) {
+		var tmpBool = whiteList.indexOf(_origin) !== -1;
+		_callback(null, tmpBool);
+		// if (whiteList.indexOf(_origin) !== -1) {
+		// 	_callback(null, true);
+		// } else {
+		// 	_callback(new Error('Not allowed by CORS'));
+		// }
+	},	
+	credentials: true
+	// origin:	whiteList,
 };
 
-// app.all('*', function (req, res, next) {
-// 	var origin = corsOption.origin.indexOf(req.header('host').toLowerCase()) > -1 ? req.headers.origin : corsOption.default;
-// 	res.header("Access-Control-Allow-Origin", origin);
-// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// 	next();
-// });
+app.use([
+	cors(corsOption),
+	express.static('client')	
+]);
 
-// app.get('/products/:id', cors(corsOption), function (req, res, next) {
+// app.get('/', cors(corsOption), function (req, res, next) {
 // 	res.json({msg: 'This is CORS-enabled for a Single Route'})
 // });
 
-
-// app.all('/*', function(req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-// 	next();
-// });
-
-app.use(express.static('client'));
-
-app.use(function (req, res, next) {
-	for (var i=0; i<whiteList.length; i++) {
-		res.header("Access-Control-Allow-Origin", whiteList[i]);
-		res.header("Access-Control-Allow-Headers", "X-Requested-With");
-		res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		if (i === whiteList.length - 1) {
-			next();
-		}
-	}
-});
-
+// set client relative path for the full service
 app.get('/', (req, res) => {
 	// __dirname => auto generated var (relative path)
 	res.sendFile(__dirname + '/index.html');
