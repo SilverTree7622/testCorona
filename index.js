b5b8d8a924d67ace7b7a0eae58c5150e79e349ff
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2020-05-02 08:56:27 +0900

    add some comment

--- index.js
+++ index.js
@@ -9,45 +9,45 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-// allow 'CORS-ORIGIN' to the only specific list
-var whiteList = [
-	'https://www.mohw.go.kr/react/popup_200128.html',
-	'https://www.mohw.go.kr/react/popup_200128_3.html',
-	'https://www.mohw.go.kr/react/popup_200128_4.html'
-];
-var corsOption = {
-	// origin: function (_origin, _callback) {
-	// 	var tmpBool = whiteList.indexOf(_origin) !== -1;
-	// 	_callback(null, tmpBool);
-	// },
-	origin:	whiteList,
-	// origin:	'http://localhost:' + port,
-	credentials: true,
-	optionsSuccessStatus: 200,
-	// methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-	// allowedHeaders: ["Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"]
-};
+// // allow 'CORS-ORIGIN' to the only specific list
+// var whiteList = [
+// 	'https://www.mohw.go.kr/react/popup_200128.html',
+// 	'https://www.mohw.go.kr/react/popup_200128_3.html',
+// 	'https://www.mohw.go.kr/react/popup_200128_4.html'
+// ];
+// var corsOption = {
+// 	// origin: function (_origin, _callback) {
+// 	// 	var tmpBool = whiteList.indexOf(_origin) !== -1;
+// 	// 	_callback(null, tmpBool);
+// 	// },
+// 	origin:	whiteList,
+// 	// origin:	'http://localhost:' + port,
+// 	credentials: true,
+// 	optionsSuccessStatus: 200,
+// 	// methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+// 	// allowedHeaders: ["Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"]
+// };
 
-// app.options('*', cors(corsOption)) // include before other routes
+// // app.options('*', cors(corsOption)) // include before other routes
 
 
 // app.use(cors(corsOption));
 app.use(express.static('client'));
 
-// set client relative path for the full service
+// set client relative path for the full service (send static {html, css, js} file)
 app.get('/', (req, res) => {
 	console.log('get function in server');
 	// __dirname => auto generated var (relative path)
 	res.sendFile(__dirname + '/index.html');
 });
 
+// socket io connection event (send this to client)
 io.on('connection', (socket) => {
-	console.log('a user connected in server io!');
 	
-	// 처음 클라이언트에 방출할 때 필요한 작업 (JSON 형식으로)
+	// init emit to client (in JSON format)
 	socket.emit('initConnection', {serverData: '서버 작동'});
 
-	// send requested HTML list
+	// send requested HTML list (hospital, center, drive info)
 	socket.on('list', () => {
 		socket.emit('listData', htmlList());
 	});
@@ -58,7 +58,8 @@ io.on('connection', (socket) => {
 	});
 });
 
+// http server (made by app)
 http.listen(port, () => {
-	console.log('__dirname:', __dirname);
+	console.log('__dirname:', __dirname); // relative direct path
 	console.log('Example app listening at http://localhost:' + port);
 });
